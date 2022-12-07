044a2192c9daef960cc138ec9fc65dc32aa9c2af
DariaBatiuk <dariabatiuk5@gmail.com>
2022-12-07 14:57:50 -0400

    LS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=5&appid=1151aaa47b533c1e5a2b281eebc6f2a6')
-.then(function (resp) {return resp.json() }) //convert data to json
-.then(function(data) {
-    console.log(data);
+// fetch('http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=5&appid=1151aaa47b533c1e5a2b281eebc6f2a6')
+// .then(function (resp) {return resp.json() }) //convert data to json
+// .then(function(data) {
+//     console.log(data);
+// })
+// .catch(function () {
+//     //catch any errors
+// });
+
+let cityName = document.querySelector('#cityName');
+let searchBTn = document.querySelector('#search-btn');
+
+console.log(cityNameValue);
+console.log(cityObj);
+console.log(saveCity);
+
+function saveCityName (cityNameValue) {
+    let cityObj = {
+        name: cityNameValue
+    }
+    console.log(cityObj);
+
+    let saveCity = JSON.parse(window.localStorage.getItem ("location") || "[]");
+    saveCity.push(cityObj);
+    console.log(saveCity);
+
+    window.localStorage.setItem("location", JSON.stringify(saveCity));
+}
+
+//click on search button
+
+searchBTn.addEventListener("click", () => {
+    let cityNameValue = cityName.value.trim();
+    console.log(cityNameValue);
+    saveCityName(cityNameValue);
 })
-.catch(function () {
-    //catch any errors
-});
+
